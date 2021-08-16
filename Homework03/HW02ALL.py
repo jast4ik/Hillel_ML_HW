dac7fe072687eb7e27f61702b3313648851b317d
Yevgen Iliashchienko <yevgen.iliashchienko@gmail.com>
2021-08-16 19:04:52 +0300

    Task 02, 03 complete.

--- Homework03/HW02ALL.py
+++ Homework03/HW02ALL.py
@@ -3,7 +3,7 @@
     The program stops working when you enter a negative number.
 #2. The number of numbers to enter and the numbers themselves are given. Display the maximum among the entered numbers.
 #3. Given a "+" or "*" sign, the number of numbers and the numbers themselves.
-    Display yhe sum or product of numbers depending on the entered sign.
+    Display the sum or product of numbers depending on the entered sign.
 #4. Given a number N. Print first N numbers of the Fibanacci sequence.
 #5. Given a number N. Print a sequence of Fibonacci numbers less than N.
 #6. Write a program that builds a correspondence table between Celsius and Fahrenheit.
@@ -26,15 +26,23 @@
 
 import math
 
-# region Input strings definition
-prompts = {
-    1: "Enter the number. Enter a negative number to stop typing."
+# region Input and result strings definition
+prompt_strings = {
+    1:  "\nEnter a number to calculate the square root.",
+    2:  "\nEnter a sequence of numbers.",
+    3:  "\nEnter the symbol of the mathematical operation and the sequence of numbers to which it should be applied."
+}
+
+result_strings = {
+    1:  "The square root of the entered number is {}.",
+    2:  "The maximum in the sequence is the number {}.",
+    3:  "The result of a mathematical operation on all elements of the sequence is {}."
 }
 
 
 # endregion
 
-# region Check input for string
+# region Auxiliary functions
 def is_string(prefix, input_string=None):
     """
     The function checks if the input variable is a string.
@@ -55,44 +63,181 @@ def is_string(prefix, input_string=None):
         return False
 
 
-# endregion
-
-
-# region Extract numbers from string.
-def extract_numbers(input_string=None):
+def extract_numbers(prefix, input_string=None, skip_element_at_index=None):
     """
     The function converts a string to a list of numbers.
+    :param prefix:
+        The prefix for the error message string.
     :param input_string:
         String to be processed.
+    :param skip_element_at_index
+        Skip element at specified index.
     :return:
         List of numbers.
     """
+    if not is_string(prefix, input_string):
+        print(str(prefix) + "\tThe input variable must be a string.")
+        return None
+
     list_of_numbers = []
 
-    for sub_string in input_string.stip().split():
-        if sub_string.isnumeric():
-            list_of_numbers.append(float(sub_string))
+    for index, sub_string in enumerate(input_string.strip().split()):
+        try:
+            if skip_element_at_index is not None:
+                if index != skip_element_at_index:
+                    list_of_numbers.append(float(sub_string))
+            else:
+                list_of_numbers.append(float(sub_string))
+
+        except (ValueError, TypeError):
+            print(str(prefix) + "\tCan't convert {} to number.".format(str(sub_string)))
+
+    if len(list_of_numbers) == 0:
+        print(str(prefix) + "\tThere is no numbers in the input line.")
+        return None
 
     return list_of_numbers
+
+
+def get_task_to_run(input_string=None):
+    """Returns the number of task to run.
+
+    :param input_string:
+        String to be processed.
+    :return:
+        Integer: Number of the task.
+    """
+    if not is_string("get_task_to_run():", input_string):
+        return None
+
+    input_string = input_string.strip()
+
+    try:
+        number = int(input_string)
+    except (ValueError, TypeError):
+        print("get_task_to_run():\t{} is not a number.".format(str(input_string)))
+        return None
+
+    if number < 0 or number > 15:
+        print("get_task_to_run():\tPlease, enter number between 1 and 15.")
+        return None
+
+    return int(number)
 
 
 # endregion
 
 # region Task #1
-def get_square_root(prefix, input_string=None):
-    if not is_string(input_string):
-        print(str(prefix) + "get_square_root():\tThe input variable must be a string.")
+def get_square_root(input_string=None):
+    """
+    The function returns the square root of the entered number.
+
+    :param input_string:
+        String to be processed.
+    :return:
+        Float. Square root of a number.
+    """
+    nums = extract_numbers("get_square_root():", input_string)
+    if nums is None:
         return None
 
-    nums = extract_numbers(input_string)
-
     if len(nums) != 1:
-        print(str(prefix) + "get_square_root():\tPlease enter only ONE number.")
+        print("get_square_root():\tPlease enter only ONE number.")
         return None
 
     if nums[0] < 0.0:
-        print(str(prefix) + "get_square_root():\tPlease enter only POSITIVE number.")
+        print("get_square_root():\tPlease enter only POSITIVE number.")
         return None
 
     return round(math.sqrt(nums[0]), 3)
+
+
+# endregion
+# region Task 02
+def max_in_sequence(input_string=None):
+    """
+    The function returns the maximum number entered.
+
+    :param input_string:
+        String to be processed.
+    :return:
+        Float:  Maximum in sequence.
+    """
+    nums = extract_numbers("max_in_sequence():", input_string)
+    if nums is None:
+        return None
+
+    return max(nums)
+
+
+# endregion
+
+
+# region Task 03
+def apply_math_to_list(input_string=None):
+    """
+    The function returns the sum or product of all the elements in the list.
+    :param input_string:
+        String to be processed.
+    :return:
+        Float: Sum or production of all elements in list.
+    """
+    operators = ("+", "*")
+
+    if not is_string("apply_math_to_list():", input_string):
+        return None
+
+    operator = input_string.strip().split()[0]
+
+    if operator not in operators:
+        print("apply_math_to_list():\t Valid operators is {}.".format(str(operators)))
+        return None
+
+    nums = extract_numbers("apply_math_to_list():", input_string, 0)
+
+    if len(nums) < 2:
+        print("apply_math_to_list():\tThere should be at least TWO numbers.")
+        return None
+
+    if operator == "+":
+        return round(sum(nums), 3)
+    elif operator == "*":
+        return round(math.prod(nums), 3)
+    else:
+        return None
+# endregion
+
+
+# region Functions dictionary definition
+functions = {
+    1:  get_square_root,
+    2:  max_in_sequence,
+    3:  apply_math_to_list
+}
+# endregion
+
+# region __main__
+if __name__ == "__main__":
+    while True:
+        user_input = input("\nPlease enter task number to run or 'q' to exit:\n")
+        if user_input.lower().strip() == "q":
+            break
+
+        print("Please, note that all values are entered on one line and separated by a space.\n"
+              + "('q' to return to the task selection).\n")
+
+        task_to_run = get_task_to_run(user_input)
+        if task_to_run is None:
+            continue
+
+        while True:
+            user_input = input(prompt_strings[task_to_run] + " ('q' to return to the task selection):\n")
+            if user_input.lower().strip() == "q":
+                break
+
+            result = functions[task_to_run](user_input)
+            if result is None:
+                continue
+
+            print(result_strings[task_to_run].format(str(result)))
 # endregion
