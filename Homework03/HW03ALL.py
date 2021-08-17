091a3c2aaf89e3e11e7b05cbbe7dcbc1fabd20d3
USER_ <yevgen.iliashchienko@gmail.com>
2021-08-17 07:37:09 +0300

    Bugfix.

--- Homework03/HW03ALL.py
+++ Homework03/HW03ALL.py
@@ -1,32 +1,35 @@
 """Hillel Machine Learning course. Homework 03.
-#1. Write a program that reads a number, prints its square root and takes the next number.
-    The program stops working when you enter a negative number.
-#2. The number of numbers to enter and the numbers themselves are given. Display the maximum among the entered numbers.
-#3. Given a "+" or "*" sign, the number of numbers and the numbers themselves.
-    Display the sum or product of numbers depending on the entered sign.
-#4. Given a number N. Print first N numbers of the Fibanacci sequence.
-#5. Given a number N. Print a sequence of Fibonacci numbers less than N.
-#6. Write a program that builds a correspondence table between Celsius and Fahrenheit.
-    The starting and ending values are given in degrees Celsius, step.
-    Display the correspondence table.
-#7. The number N is given. After that, in the order of traversal, N pairs of coordinates of the polygon on the plane
-    are entered. Find the perimeter of this polygon.
-#8. The number N is given and a list of N numbers. For each number from the list, output its ratio to the arithmetic
-    mean of this list (less, more, equal).
-#9. The number N is given and a list of N numbers. Print all numbers greater than both of their neighbors in the list.
-#10.The number N is given and a list of N numbers. Display the list in reverse order.
-#11.The number N is given and a list of N numbers. Further, pairs of numbers (A, B) greater than zero are given to the
-    input. For each pair, print the values of the list with indices from A (inclusive) to B (not inclusive). Exit on
-    entering two zeros. Consider the case when A>B.
-#12.The number N is given and a list of N numbers. Print all numbers that appear in the list only once.
-#13.The number N is given and a list of N numbers. The number M is given and a list of M numbers.
-    Print all numbers that are in both lists.
-#14.Implement a CRM system that should accept and process the following types of requests:
-        - add employee named <name> to the system (add);
-        - check if there is an employee named <name> in the system (find);
-        - list all employees (list);
-        - remove employee named <name> from the system (delete);
-        - shit down the system (stop).
+#1.     Write a program that reads a number, prints its square root and takes the next number.
+        The program stops working when you enter a negative number.
+#2.     The number of numbers to enter and the numbers themselves are given. Display the maximum among the entered numbers.
+#3.     Given a "+" or "*" sign, the number of numbers and the numbers themselves.
+        Display the sum or product of numbers depending on the entered sign.
+#4.     Given a number N. Print first N numbers of the Fibanacci sequence.
+#5.     Given a number N. Print a sequence of Fibonacci numbers less than N.
+#6.     Write a program that builds a correspondence table between Celsius and Fahrenheit.
+        The starting and ending values are given in degrees Celsius, step.
+        Display the correspondence table.
+#7.     The number N is given. After that, in the order of traversal, N pairs of coordinates of the polygon on the plane
+        are entered. Find the perimeter of this polygon.
+#8.     The number N is given and a list of N numbers. For each number from the list, output its ratio to the arithmetic
+        mean of this list (less, more, equal).
+#9.     The number N is given and a list of N numbers. Print all numbers greater than both of their neighbors in the list.
+#10.    The number N is given and a list of N numbers. Display the list in reverse order.
+#11.    The number N is given and a list of N numbers. Further, pairs of numbers (A, B) greater than zero are given to the
+        input. For each pair, print the values of the list with indices from A (inclusive) to B (not inclusive). Exit on
+        entering two zeros. Consider the case when A>B.
+#12.    The number N is given and a list of N numbers. Print all numbers that appear in the list only once.
+#13.    The number N is given and a list of N numbers. The number M is given and a list of M numbers.
+        Print all numbers that are in both lists.
+#14.    Implement a CRM system that should accept and process the following types of requests:
+            - add employee named <name> to the system (add);
+            - check if there is an employee named <name> in the system (find);
+            - list all employees (list);
+            - remove employee named <name> from the system (delete);
+            - shit down the system (stop).
+#15.    Task from www.codewars.com
+        https://www.codewars.com/kata/5263a84ffcadb968b6000513
+
 """
 
 import math
