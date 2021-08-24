1c1296eb88560a2d7fbe495e47586b1c02583393
Yevgen Iliashchienko <yevgen.iliashchienko@gmail.com>
2021-08-24 22:24:40 +0300

    Bugfix.

--- Homework04/HW04ALL.py
+++ Homework04/HW04ALL.py
@@ -3,6 +3,7 @@
 import AгxiliaryFunctions as af
 import numpy as np
 import time
+import gc
 
 __author__ = "Yevgen Iliashchienko"
 __copyright__ = "Copyright 2021, Yevgen Iliashchienko"
@@ -14,25 +15,30 @@ __status__ = "Development"
 
 
 # region Task01
-def get_col_sum(input_string: str, separator_id=1):
+def get_col_sum(dimension: tuple, value: float):
 
-    if not af.is_string("get_col_sum():\t", input_string):
+    if dimension[0] > 10000 or dimension[1] > 10000:
+        print("There is no memory check, so size of array is limited to 10000.")
         return None
 
-    input_data = af.extract_tuples("get_col_sum():\t", input_string, separator_id)[0]
-    if input_data is None:
-        return None
+    python_list = []
 
-    for row_index, row in enumerate(input_data[0:-2:1]):
-        if len(row) != len(input_data[row_index + 2]):
-            return None
+    print("Creating Python list...")
+    start_time = time.time()
+    for row_index in range(dimension[1]):
+        python_list.append([])
+        for col_index in range(dimension[0]):
+            python_list[row_index].append(value)
+
+    python_creation_time = time.time() - start_time
+    print("Time to proceed: {:.10f}s.".format(python_creation_time))
 
     col_sums = {}
 
-    print("\nSolution using Python list...")
+    print("\nCalculating sum of Python list with for loop...")
     start_time = time.time()
 
-    for row in input_data:
+    for row in python_list:
         for col_index, col_element in enumerate(row):
             if col_index not in col_sums:
                 col_sums[col_index] = col_element
@@ -42,13 +48,25 @@ def get_col_sum(input_string: str, separator_id=1):
     python_time = time.time() - start_time
     print("Time to proceed: {:.10f}s.".format(python_time))
 
-    numpy_array = np.array(input_data)
-    print("Solution using NumPy numpy.sum()...")
+    print("\nCreating NumPy array...")
+    start_time = time.time()
+    numpy_array = np.full(dimension, value, dtype=float)
+    numpy_creation_time = time.time() - start_time
+    print("Time to proceed: {:.10f}s.".format(numpy_creation_time))
+
+    print("\nCalculating sum of NumPy with numpy.sum()...")
     start_time = time.time()
     sums = np.sum(numpy_array, axis=0)
     numpy_time = time.time() - start_time
     print("Time to proceed: {:.10f}s.".format(numpy_time))
-    af.print_time_relation("NumPy np.sum()", numpy_time, python_time)
+    numpy_array = None
+    af.print_time_relation("\nNumPy array creation ", numpy_creation_time, python_creation_time)
+    af.print_time_relation("NumPy np.sum() ", numpy_time, python_time)
+
+    print("\nCleaning up memory...")
+    del numpy_array
+    del python_list
+    gc.collect()
 
     return sums
 
@@ -59,15 +77,10 @@ if __name__ == "__main__":
 
     print("Task 01...")
     print("\n----------10x10 matrix----------")
-    a = np.full((30, 30), 4)
-    print(get_col_sum(np.array2string(a), 2))
+    get_col_sum((10, 10), 2)
     print("\n----------100x100 matrix----------")
-    a = np.full((100, 100), 4)
-    print(get_col_sum(np.array2string(a), 2))
+    get_col_sum((100, 100), 2)
     print("\n----------1000x1000 matrix----------")
-    a = np.full((1000, 1000), 4)
-    print(get_col_sum(np.array2string(a), 2))
+    get_col_sum((1000, 1000), 2)
     print("\n----------10000x10000 matrix----------")
-    a = np.full((10000, 10000), 4)
-    print(get_col_sum(np.array2string(a), 2))
-
+    get_col_sum((10000, 10000), 2)
