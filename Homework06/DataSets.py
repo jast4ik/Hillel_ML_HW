ede0c0a176a0939608724f9433871bcac681749e
Yevgen Iliashchienko <yevgen.iliashchienko@gmail.com>
2021-09-12 23:05:29 +0300

    HW06 Bugfix

--- Homework06/DataSets.py
+++ Homework06/DataSets.py
@@ -1,6 +1,8 @@
+import struct
 from os.path import isfile
 import pandas as pd
 import gc
+import re
 import numpy as np
 from scipy.stats import zscore
 
@@ -44,30 +46,53 @@ def get_clear_df():
     return clear_df
 
 
-def get_lable_encoded_clear_df():
+def get_lable_encoded_clear_df(regenerate=False):
     result_df = pd.DataFrame()
 
-    if isfile("./data/clear_label_encoded_df.pkl"):
+    if isfile("./data/clear_label_encoded_df.pkl") and not regenerate:
         result_df = pd.read_pickle(filepath_or_buffer="./data/clear_label_encoded_df.pkl", compression='zip')
         print("\nLabel encoded dataframe loaded.")
     else:
         clear_df = get_clear_df()
 
+        clear_df['Model'] = clear_df['Model'].map(
+            lambda x: "".join(re.findall("[a-zA-Z0-9]+", x, flags=re.IGNORECASE))
+        )
+        clear_df['Model'] = clear_df['Model'].map(str.upper)
+
+        clear_df['Make'] = clear_df['Make'].map(
+            lambda x: "".join(re.findall("[a-zA-Z]+", x, flags=re.IGNORECASE))
+        )
+        clear_df['Make'] = clear_df['Make'].map(str.upper)
+
+        clear_df['City'] = clear_df['City'].map(
+            lambda x: "".join(re.findall("[a-zA-Z]+", x, flags=re.IGNORECASE))
+        )
+        clear_df['City'] = clear_df['City'].map(str.upper)
+
+        clear_df['State'] = clear_df['State'].map(
+            lambda x: "".join(re.findall("[a-zA-Z]+", x, flags=re.IGNORECASE))
+        )
+        clear_df['State'] = clear_df['State'].map(str.upper)
+
         make_cat = pd.DataFrame()
         model_cat = pd.DataFrame()
+        make_model_cat = pd.DataFrame()
         state_cat = pd.DataFrame()
         city_cat = pd.DataFrame()
-        make_cat['Make_cat'] = clear_df['Make'].astype('category').cat.codes
-        model_cat['Model_cat'] = clear_df['Model'].astype('category').cat.codes
+        #make_cat['Make_cat'] = clear_df['Make'].astype('category').cat.codes
+        #model_cat['Model_cat'] = clear_df['Model'].astype('category').cat.codes
         state_cat['City_cat'] = clear_df['City'].astype('category').cat.codes
         city_cat['State_cat'] = clear_df['State'].astype('category').cat.codes
 
         result_df = clear_df.select_dtypes(include=['int64', 'float64'])
-        result_df = result_df.join(model_cat)
-        result_df = result_df.join(make_cat)
+        #result_df = result_df.join(model_cat)
+        #result_df = result_df.join(make_cat)
+        make_model_cat['Make_Model'] = clear_df['Make'] + clear_df['Model']
+        make_model_cat['Make_Model'] = make_model_cat['Make_Model'].astype('category').cat.codes
         result_df = result_df.join(city_cat)
         result_df = result_df.join(state_cat)
-
+        result_df = result_df.join(make_model_cat)
 
         del clear_df
         del make_cat
@@ -82,5 +107,6 @@ def get_lable_encoded_clear_df():
 
 if __name__ == "__main__":
     pass
-    src_df = get_lable_encoded_clear_df()
+    src_df = get_lable_encoded_clear_df(regenerate=True)
+    print(src_df['Make_Model'].max())
     print(src_df.head)
--- Homework06/xgb.py
+++ Homework06/xgb.py
@@ -8,17 +8,38 @@ from os.path import isfile
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 import DataSets as ds
+import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
     encoded_df = ds.get_lable_encoded_clear_df()
 
-    X = encoded_df[['Year', 'Mileage', 'Model_cat', 'Make_cat']]
+    upper_limit = encoded_df['Price'].quantile(0.995)
+    bottom_limit = encoded_df['Price'].quantile(0.005)
+
+    print(upper_limit, bottom_limit)
+
+    encoded_df = encoded_df[encoded_df['Price'].between(bottom_limit, upper_limit)]
+
+    upper_limit = encoded_df['Mileage'].quantile(0.995)
+    bottom_limit = encoded_df['Mileage'].quantile(0.005)
+
+    print(upper_limit, bottom_limit)
+
+    encoded_df = encoded_df[encoded_df['Mileage'].between(bottom_limit, upper_limit)]
+
+    print(encoded_df.head)
+
+    X = encoded_df[['Year', 'Mileage', 'Make_Model', 'State_cat', 'City_cat']]
     y = encoded_df['Price']
 
-    X = X.apply(scipy.stats.zscore)
-    y = y.apply(np.log)
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.8, random_state=42)
+
+    #X = X.apply(scipy.stats.zscore)
+    y = y.apply(np.log)
+    #X['Mileage'] = X['Mileage'].map(np.log)
+    #print(X.head)
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.8, random_state=2)
 
     dmatrix_train = xgb.DMatrix(data=X_train, label=y_train)
     dmatrix_test = xgb.DMatrix(data=X_test, label=y_test)
@@ -35,3 +56,6 @@ if __name__ == "__main__":
     r2 = r2_score(y_test, xgb_preds)
 
     print("Best r2: ", r2)
+
+    xgb.plot_importance(xgb_model)
+    plt.show()
